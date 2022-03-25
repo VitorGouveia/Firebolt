d252368218564b22d3b1e50efc65b8761666d402
Vitor Gouveia <vitorneves.gouveia10@gmail.com>
2022-03-24 23:44:34 -0300

    feat(components): add create method with some kind of two way data binding

--- examples/index.js
+++ examples/index.js
@@ -1,5 +1,56 @@
 const firebolt = {
   hi: "vitor",
+  create(tag, controllerCallback) {
+    const { component, ...props } = controllerCallback();
+
+    const ID = new Date().getTime();
+
+    class Element extends HTMLElement {
+      constructor() {
+        const self = super();
+
+        Object.entries(props).forEach(([name, value]) => {
+          self[name] = value;
+        });
+
+        self.setAttribute("data-firebolt-id", ID);
+
+        return self;
+      }
+
+      static get observedAttributes() {
+        return Object.keys(props);
+      }
+
+      attributeChangedCallback(name, oldValue, newValue) {
+        this[name] = newValue;
+      }
+
+      connectedCallback() {
+        if (typeof tag === "string") {
+          this.innerHTML = `<${tag}>${this.textContent}</${tag}>`;
+        }
+      }
+
+      // disconnectedCallback() {}
+    }
+
+    customElements.define(component, Element);
+
+    return document.querySelector(`[data-firebolt-id="${ID}"]`);
+  },
+  createContext(callback) {
+    const context = callback();
+
+    return () => context;
+  },
+  useContext(context, selector) {
+    if (selector) {
+      return selector(context());
+    }
+
+    return context();
+  },
   createStore(callback) {
     const store = callback();
 
